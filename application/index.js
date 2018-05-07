a5a497d50343e8559054366cb2a07de36e39b7bf
Guilherme Pontes <guilhermepontes@msn.com>
2018-05-07 15:36:53 +0200

    Update index.js

--- application/index.js
+++ application/index.js
@@ -1,17 +1,13 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import { Provider } from 'react-redux'
 import { View } from 'react-native'
 import store from '@application/store'
 import Main from '@screens/Main'
 
-class Application extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <Main />
-      </Provider>
-    )
-  }
-}
+const App = () => (
+  <Provider store={store}>
+    <Main />
+  </Provider>
+)
 
 export default Application
